cc12948e32ef390b7646ce349a50374f07e82710
raed <52961364+gitRaed@users.noreply.github.com>
2022-05-09 14:01:32 +0100

    navbar css done
    still need to do the css for mobiles devices

--- src/components/content/disclaimerPage.js
+++ src/components/content/disclaimerPage.js
@@ -3,7 +3,8 @@ export function DisclaimerPage() {
         <div className="disclaimerPage">
             This website is for training purpose only and to hone my skills. 
             The articles present on this website aren’t sell, the original links of those articles will be shown. 
-            Please pay a visit to the originals shops if you’re interested in an article.
+            Please pay a visit to the originals shops if you’re interested in an article. 
+            Socials links are not real one to, since it’s not a real online shop !
         </div>
     )
 }
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,22 +1,31 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInstagram, faTwitter, faFacebookF } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+
+import '../styles/navbar.css';
 const img = require('../imgs/logo/logo.PNG');
 
 export function Navbar (props){
 
     return(
-        <div>
-            <div className='img-div'>
+        <nav className='div-navbar'>
+            <div className='img-div '>
                 <img src={img} alt='logo'/>
             </div>
-            <nav>
-                <ul>
-                    <li onClick={() => {props.setDisplay(1)}}>Homepage</li>
-                    <li onClick={() => {props.setDisplay(2)}}>T-shirts</li>
-                    <li onClick={() => {props.setDisplay(3)}}>Hoodies</li>
-                    <li onClick={() => {props.setDisplay(4)}}>Prints</li>
-                    <li onClick={() => {props.setDisplay(5)}}>Others</li>
-                    <li onClick={() => {props.setDisplay(6)}}>About</li>
-                </ul>
-            </nav>
-        </div>
+            <ul>
+                <li><span onClick={() => {props.setDisplay(1)}}>Homepage</span></li>
+                <li><span onClick={() => {props.setDisplay(2)}}>T-shirts</span></li>
+                <li><span onClick={() => {props.setDisplay(3)}}>Hoodies</span></li>
+                <li><span onClick={() => {props.setDisplay(4)}}>Prints</span></li>
+                <li><span onClick={() => {props.setDisplay(5)}}>Others</span></li>
+                <li><span onClick={() => {props.setDisplay(6)}}>About</span></li>
+            </ul>
+            <div className='socials'>
+                <a href='https://www.instagram.com'><FontAwesomeIcon className='socials-icon' icon={faInstagram} /></a>
+                <a href='https://www.twitter.com'><FontAwesomeIcon className='socials-icon' icon={faTwitter}/></a>
+                <a href='https://www.facebook.com'><FontAwesomeIcon className='socials-icon' icon={faFacebookF} /></a>
+            </div>
+
+        </nav>
     )
 }
